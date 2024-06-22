2ee27fe58ba39c211294bd654f59c978317a7e21
Samuel Chen <sche842@gmail.com>
2024-06-22 17:06:25 +1000

    pokeCard rendering added

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,34 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { getPokemon } from "../../services/pokeApi";
+function PokeCard({ pokemon }) {
+  // add the zeros in front
+  const paddedId = pokemon.id.toString().padStart(3, "0");
 
-function PokeCard() {
-  const [pokemons, setPokemons] = useState([]);
-
-  useEffect(() => {
-    const fetchPokemon = async () => {
-      try {
-        const pokemonData = await getPokemon();
-        setPokemons(pokemonData);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    fetchPokemon();
-  }, []);
-
-  console.log(pokemons[1]);
   return (
     <div className="cursor-pointer w-[px] h-[px] block relative bg-red-500 my-1 mx-2 py-2 px-5">
       <div></div>
       <div className="w-[130px] h-[160px] bg-slate-200 rounded">
-        <span>#0001</span>
-        <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/001.png" />
+        <span>#{paddedId}</span>
+        <img
+          src={`https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/${paddedId}.png`}
+        />
       </div>
       <div>
         <div>
-          <span className="text-xl">Bulbasaur</span>
+          <span className="text-xl">{pokemon.name}</span>
         </div>
         <div className="flex justify-between">
           <span>Grass</span>
--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokeCard from "./PokeCard";
+import { getPokemon } from "../../services/pokeApi";
 
 function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+  // loading usestate?
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const pokemonData = await getPokemon();
+        setPokemons(pokemonData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchPokemon();
+  }, []);
+
+  console.log(pokemons[1]);
   return (
     <div className="bg-green-500 h-[500px] py-2">
       <div className="flex flex-wrap justify-center">
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
+        {pokemons.map((pokemon) => (
+          <PokeCard key={pokemon.id} pokemon={pokemon} />
+        ))}
       </div>
     </div>
   );

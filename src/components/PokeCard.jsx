8523f648fdcb414a25abf235e349466b6d50fae4
Samuel Chen <sche842@gmail.com>
2024-06-23 18:22:16 +1000

    type colours added

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -8,10 +8,12 @@ function PokeCard({ pokemon }) {
 
   //   console.log(pokemon.type[0].type.name);
 
+  // make the card a % width and also set a max or min width
+  // @media query for a max-width for pokedex
+
   return (
-    <div className="cursor-pointer w-[px] h-[px] block relative bg-red-200 my-1 mx-1 py-2 px-5">
-      <div></div>
-      <div className="w-[130px] h-[160px] bg-slate-100 rounded">
+    <div className="cursor-pointer h-[px] block relative bg-red-200 my-1 mx-1 py-2 px-5">
+      <div className="min-w-[130px] max-w-[200px] h-[200px] bg-slate-100 rounded">
         <span className="pl-2">#{paddedId}</span>
         <img
           src={`https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/${paddedId}.png`}
@@ -23,9 +25,15 @@ function PokeCard({ pokemon }) {
         </div>
         <div className="flex justify-between capitalize">
           {/* [perhaps make this map] */}
-          <span className={`${type}`}>{type}</span>
+          <span
+            className={`${type} font-semibold rounded-md shadow-md w-[38%]`}
+          >
+            {type}
+          </span>
           {pokemon.type[1]?.type?.name && (
-            <span className="dragon">{pokemon.type[1].type.name}</span>
+            <span className="water font-semibold rounded-md shadow-md w-[38%]">
+              {pokemon.type[1].type.name}
+            </span>
           )}
         </div>
       </div>
@@ -36,3 +44,5 @@ function PokeCard({ pokemon }) {
 export default PokeCard;
 
 // set the use effect in pokedex instead, and pass the children in while for each
+
+// !!! something is wrong with generating the cards,
--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -4,15 +4,18 @@ import { getPokemon } from "../../services/pokeApi";
 
 function Pokedex() {
   const [pokemons, setPokemons] = useState([]);
-  // loading usestate?
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const fetchPokemon = async () => {
       try {
         const pokemonData = await getPokemon();
         setPokemons(pokemonData);
+        setLoading(false);
       } catch (err) {
         console.log(err);
+        setLoading(false);
       }
     };
 
@@ -22,11 +25,16 @@ function Pokedex() {
   console.log(pokemons[1]);
   return (
     <div className="bg-green-500 py-2 px-2">
-      <div className="flex flex-wrap justify-center bg-blue-600 w-auto">
-        {pokemons.map((pokemon) => (
-          <PokeCard key={pokemon.id} pokemon={pokemon} />
-        ))}
-      </div>
+      {loading ? (
+        // add a spinning circle?
+        <div>Loading...</div>
+      ) : (
+        <div className="flex flex-wrap justify-center bg-blue-600 w-auto">
+          {pokemons.map((pokemon) => (
+            <PokeCard key={pokemon.id} pokemon={pokemon} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }

fe86fdb6b8d6cdc8e3b774b9da4b16eaab185317
Samuel Chen <sche842@gmail.com>
2024-07-08 14:53:49 +1000

    ui tweaks

--- src/components/modal/PokeModal.jsx
+++ src/components/modal/PokeModal.jsx
@@ -77,7 +77,7 @@ function PokeModal({
         </div>
       ) : (
         <div
-          className={`overflow-hidden bg-red-500 max-w-[700px] sm:mx-6 sms:px-4 rounded-xl h-[100vh] xl:h-[95%] relative outline`}
+          className={`overflow-hidden bg-red-500 max-w-[700px] sm:mx-6 sms:px-4 rounded-xl h-dvh xl:h-[95%] relative outline`}
           onClick={(event) => event.stopPropagation()}
         >
           <div className="">
